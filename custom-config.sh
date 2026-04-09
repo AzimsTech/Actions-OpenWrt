a81f991eb8179a34f55a5c0afab044843a9d1bcd
Wan Azims <azimstech@gmail.com>
2026-04-09 19:30:33 +0800

    Add CONFIG_MT76_LEDS to kernel configuration

--- custom-config.sh
+++ custom-config.sh
@@ -1,5 +1,6 @@
 # enable kernel testing
 echo 'CONFIG_TESTING_KERNEL=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_MT76_LEDS=y' >> $BUILD_ROOT/.config
 # enable all kernel modules
 # echo 'CONFIG_ALL_KMODS=y' >> $BUILD_ROOT/.config
 
